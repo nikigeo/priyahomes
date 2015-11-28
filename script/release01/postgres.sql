8fca18fbc4e6665750bc8dfe81f37c9dad338564
pvarghese <pradeeshvarghese@gmail.com>
2015-11-29 00:39:23 +0530

    ISSUE9 : modified the table name.

--- script/release01/postgres.sql
+++ script/release01/postgres.sql
@@ -9,12 +9,12 @@
        LC_CTYPE = 'en_IN'
        CONNECTION LIMIT = -1;
 
-CREATE TABLE ph_internal_user (
+CREATE TABLE ph_internal_users (
 	username varchar(50) NOT NULL,
 	password varchar(100) NOT NULL,
 	enable bool,
 	CONSTRAINT ph_internal_user_pk PRIMARY KEY (username)
 );
 
-insert into ph_internal_user(username,password,enable) values('admin','$2a$10$04TVADrR6/SPLBjsK0N30.Jf5fNjBugSACeGv1S69dZALR7lSov0y',true);
-commit;
+INSERT INTO ph_internal_user(username,password,enable) VALUES('admin','$2a$10$04TVADrR6/SPLBjsK0N30.Jf5fNjBugSACeGv1S69dZALR7lSov0y',true);
+COMMIT;
